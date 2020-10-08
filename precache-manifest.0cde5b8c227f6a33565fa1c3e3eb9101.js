de051477fc407e17bfe7d50f3e06582e3d79aeec
zhangbx <zhangbx@didiglobal.com>
2020-10-08 19:13:37 +0800

    first commit

---
+++ precache-manifest.0cde5b8c227f6a33565fa1c3e3eb9101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61754750556202f67f1a278b168835aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e991af003e11545e3b4",
+    "url": "/static/css/main.a2df35d5.chunk.css"
+  },
+  {
+    "revision": "39a711a779ff66afa751",
+    "url": "/static/js/2.95c8a2ea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.95c8a2ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e991af003e11545e3b4",
+    "url": "/static/js/main.edc231f1.chunk.js"
+  },
+  {
+    "revision": "313bfc7444da2fec65d4",
+    "url": "/static/js/runtime-main.e5e8f68d.js"
+  }
+]);
